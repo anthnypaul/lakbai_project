bf9f7b5a079eb3158e57629ac75dfd6280b38143
anthnypaul <polbagabaldo@outlook.com>
2024-10-23 11:31:53 -0700

    added duration, budget, and preferences in the table

--- app/models.py
+++ app/models.py
@@ -7,5 +7,7 @@ class Itinerary(Base):
     id = Column(Integer, primary_key=True, index=True)
     city = Column(String, nullable=False)
     country = Column(String, nullable=False)
+    duration = Column(Integer, nullable=False)
+    budget = Column(Integer, nullable=False)
     description = Column(Text, nullable=False)
-
+    preferences = Column(Text, nullable=True)  
